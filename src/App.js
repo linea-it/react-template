15e3c46118c37460a0631983829202be360cdbdb
“felipelm” <felipelm123@gmail.com>
2019-03-07 11:16:32 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import theme from './theme/MaterialTheme';
+
+const API_URL =
+  process.env.NODE_ENV !== 'development'
+    ? window._env_.REACT_APP_API_URL
+    : process.env.REACT_APP_API_URL;
+
+function App() {
+  console.log(process.env)
+  return (
+    <MuiThemeProvider theme={theme}>
+      <AppBar position="static">
+        <Typography variant="h6" color="inherit">
+          AppBar API: {API_URL}
+        </Typography>
+      </AppBar>
+      <Button color="primary">Test Primary</Button>
+      <Button color="secondary">Test Secondary</Button>
+    </MuiThemeProvider>
+  );
+}
+
+export default App;
